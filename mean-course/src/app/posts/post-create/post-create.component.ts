e2e722a91fdd6818275abf74fdbe6d04d7fb220a
Eric Anastas <aireq303@gmail.com>
2018-12-04 22:48:05 -0800

    Added textarea and button to post-create component and bound this to a method that shows an alert

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { allResolved } from 'q';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html'
 })
-export class PostCreateComponent {}
+export class PostCreateComponent {
+  onAddPost() {
+    alert('Post Added!');
+  }
+}
